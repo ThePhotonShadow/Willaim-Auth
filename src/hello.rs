1c8808727a4e7974aa6552674c2d1231f3e18675
ThePhotonShadow <pnkidd@gmail.com>
2020-05-15 23:49:42 -0500

    Added simple test for warp framework.

--- src/hello.rs
+++ src/hello.rs
@@ -1,6 +1,13 @@
 extern crate sqlx;
 extern crate warp;
+use warp::Filter;
 
-fn main() {
-    println!("hello")
+async fn main() {
+    // GET /hello/warp => 200 OK with body "Hello, warp!"
+    let hello = warp::path!("hello" / String)
+        .map(|name| format!("Hello, {}!", name));
+
+    warp::serve(hello)
+        .run(([127, 0, 0, 1], 3030))
+        .await;
 }
