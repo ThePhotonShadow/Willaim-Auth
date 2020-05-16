5ed74f9fedc5644a766e6abf6643ef0c0f1b362e
ThePhotonShadow <pnkidd@gmail.com>
2020-05-16 03:17:29 -0500

    Add sqlx and introduce tokio/runtime-async conflict.

--- src/hello.rs
+++ src/hello.rs
@@ -1,9 +1,24 @@
-//extern crate sqlx;
-//extern crate warp;
+use std::env;
+use sqlx::Connect;
 use warp::Filter;
+use parking_lot::RwLock;
+use std::collections::HashMap;
+use std::sync::Arc;
+
+
+
+//fn checkCredentials (username: &str, password: &str) {
+//
+//}
+
 
 #[tokio::main]
 async fn main () {
+    let dbUname: &str = "acore";
+    let dbPwd: &str = "acore";
+    let dbpool = MySqlPool::new(format!("{}{}{}{}{}",
+        "mysql://", dbUname, ":", dbPwd, "@localhost")).await?;
+
     // GET /hello/warp => 200 OK with body "Hello, warp!"
     let hello = warp::path!("hello" / String)
         .map(|name| format!("Hello, {}!", name));
