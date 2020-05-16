220c194183c63bc19916c10d264e104f94e58680
ThePhotonShadow <pnkidd@gmail.com>
2020-05-15 22:45:03 -0500

    Added cargo.toml

---
+++ target/debug/deps/hello-e1ca4eb4bb139669.d
@@ -0,0 +1,7 @@
+C:\Users\pnkid\Desktop\Willaim-Auth\target\debug\deps\hello-e1ca4eb4bb139669.rmeta: src/hello.rs
+
+C:\Users\pnkid\Desktop\Willaim-Auth\target\debug\deps\libhello-e1ca4eb4bb139669.rlib: src/hello.rs
+
+C:\Users\pnkid\Desktop\Willaim-Auth\target\debug\deps\hello-e1ca4eb4bb139669.d: src/hello.rs
+
+src/hello.rs:
