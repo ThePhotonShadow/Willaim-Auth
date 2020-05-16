be5290964ad0b958e576944e07d118788f5f2dd9
ThePhotonShadow <pnkidd@gmail.com>
2020-05-16 16:25:24 -0500

    Corrected tokio async-std conflict by switching to Tokio in sqlx.

--- src/hello.rs
+++ src/hello.rs
@@ -5,19 +5,21 @@ use parking_lot::RwLock;
 use std::collections::HashMap;
 use std::sync::Arc;
 
-
+//async-std = {version = "1.0.0", features = ["attributes"]}
+//Above for async without tokio
 
 // fn checkCredentials (username: &str, password: &str) {
 //
 // }
-
-
-#[async_std::main] //#[tokio::main]
-async fn main () -> Result<(), sqlx::Error>{
-
-    let pool = MySqlPool::builder()
+async fn connect_db () -> Result<(), sqlx::Error>{
+    MySqlPool::builder()
         .max_size(5) // maximum number of connections in the pool
         .build("mysql://acore:acore@localhost").await?;
+    Ok(())
+}
+
+#[tokio::main]
+async fn main () -> (){
 
     // GET /hello/warp => 200 OK with body "Hello, warp!"
     let hello = warp::path!("hello" / String)
@@ -26,6 +28,4 @@ async fn main () -> Result<(), sqlx::Error>{
     warp::serve(hello)
         .run(([127, 0, 0, 1], 3030))
         .await;
-
-    Ok(())
 }
