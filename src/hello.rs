6525cfedae230e63aa1b0eca52a792aac3ec29b8
ThePhotonShadow <pnkidd@gmail.com>
2020-05-16 00:01:10 -0500

    Corrected main() to use Tokio rewrite.

--- src/hello.rs
+++ src/hello.rs
@@ -1,8 +1,9 @@
-extern crate sqlx;
-extern crate warp;
+//extern crate sqlx;
+//extern crate warp;
 use warp::Filter;
 
-async fn main() {
+#[tokio::main]
+async fn main () {
     // GET /hello/warp => 200 OK with body "Hello, warp!"
     let hello = warp::path!("hello" / String)
         .map(|name| format!("Hello, {}!", name));
